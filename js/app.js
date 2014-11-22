9d720505fb2b0e44df1e88270c8076af54d55140
Tom@Pegasus <info@thomas-mundt.de>
2014-11-22 19:50:29 +0100

    added: angularJS1.3.3, npm/bootstrap to /lib, bower installed

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by thomasmundt on 22.11.14.
+ */
+
+var initApp = angular.module('initApp',[]);
--- js/controller/controller.index.js
+++ js/controller/controller.index.js
@@ -1,6 +1,7 @@
 /**
  * Created by Tom on 10.11.2014.
+ * @
  */
 initApp.controller("IndexController", function($scope) {
-    $scope.test = "Hallo Test";
+    $scope.name = "Doktor Mc.Coy";
 });
